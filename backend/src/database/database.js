7ee605f759f1066b165917d139deb7c8f0786a62
WagnerGeroldi <wagnergeroldi@gmail.com>
2021-11-19 09:28:59 -0300

    add API

---
+++ backend/src/database/database.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose"); //lib para se conectar ao mongoDB
+
+function connect() {
+
+    mongoose.connect(
+        "mongodb://localhost:27017/api-restfull?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false"
+      );
+      
+      const db = mongoose.connection;
+      
+      db.once("open", () => {
+        console.log("Conectado ao banco de dados MONGODB");
+      });
+      
+      db.on("error", console.error.bind("Erro de conexão"));
+}
+
+module.exports = {
+  connect
+}
